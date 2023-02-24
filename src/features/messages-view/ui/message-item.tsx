747bfaab79b66918bbeeee57733939173e65a125
nikolay <killaris97@mail.ru>
2023-02-24 21:07:32 +0300

    final for socket chat messages

---
+++ src/features/messages-view/ui/message-item.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { MessageCenter, MessageRowLeft, MessageRowRight, StoreMessage } from 'entities/message';
+import { ApiChatMembersResponse } from '../consts/chat-members';
+import { performTimeSend } from '../utils';
+
+interface MessageItemProps {
+    message: StoreMessage;
+    members: ApiChatMembersResponse;
+    userId: string;
+}
+
+const MessageItem: React.FC<MessageItemProps> = ({ message, members, userId }) => {
+    const timeSend = performTimeSend(message.createdAt);
+
+    if (message.senderId === userId) {
+        return <MessageRowRight key={message.id} text={message.text} timestamp={timeSend} />;
+    }
+
+    if (message.actionId > 1) {
+        return <MessageCenter key={message.id}>{message.text}</MessageCenter>;
+    }
+
+    const username =
+        members?.list.filter(member => member.user_id === message.senderId)[0]?.username ??
+        'unknown';
+    return (
+        <MessageRowLeft
+            key={message.id}
+            username={username}
+            photoUrl={username[0].toUpperCase()}
+            text={message.text}
+            timestamp={timeSend}
+        />
+    );
+};
+
+export default MessageItem;
